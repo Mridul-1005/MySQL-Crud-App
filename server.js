51c315ba195e1e369d7f4da53f7296e0afadce81
Mridul-1005 <mhdanger193@gmail.com>
2024-11-21 09:17:22 +0600

    project initialized

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+//rest object
+const app = express()
+
+//middlewares
+//routes
+app.get('/test',(req,res)=>{
+    res.status(200).send("<h1>Nodejs Mysql APP Updated</h1>");
+})
+
+//port
+const PORT = 8080
+
+//listen
+app.listen(PORT, ()=>{
+    console.log("Server Running");
+})
