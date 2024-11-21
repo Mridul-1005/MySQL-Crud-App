0387745c5faefb4f2b5711d00acd83610adb31f6
Mridul-1005 <mhdanger193@gmail.com>
2024-11-21 09:32:53 +0600

    added middlewares

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express')
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+//configure dotenv
+dotenv.config();
 
 //rest object
 const app = express()
 
 //middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
 //routes
 app.get('/test',(req,res)=>{
     res.status(200).send("<h1>Nodejs Mysql APP Updated</h1>");
 })
 
 //port
-const PORT = 8080
+const PORT = process.env.PORT || 8000;
 
 //listen
 app.listen(PORT, ()=>{
-    console.log("Server Running");
+    console.log(`Server Running on port ${process.env.PORT}`.bgMagenta.white);
 })
